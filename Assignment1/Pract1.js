4c79562294268951688e0423e59b848723f8ba1b
Harshalak02 <109841967+Harshalak02@users.noreply.github.com>
2023-07-29 21:18:45 +0530

    Create Pract1.js

---
+++ Assignment1/Pract1.js
@@ -0,0 +1,14 @@
+let str = "Hello, World!";
+let num = 52;
+let boo = true;
+console.log("String ", str);
+console.log("Number ", num);
+console.log("Boolean ", boo);
+
+function add(a, b) {
+    return a + b;
+}
+const num1 = 10;
+const num2 = 20;
+const sum = add(num1, num2);
+console.log(sum);
